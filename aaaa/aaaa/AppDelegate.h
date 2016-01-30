4a3e23e324cd21203f4a67e557f532df0b16c678
lanouhn <962190634@qq.com>
2016-01-30 11:09:52 +0800

    123

---
+++ aaaa/aaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaa
+//
+//  Created by lanouhn on 16/1/30.
+//  Copyright © 2016年 XSteve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaa/aaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaa
+//
+//  Created by lanouhn on 16/1/30.
+//  Copyright © 2016年 XSteve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
